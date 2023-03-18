df47528c0e937cf21a6874f831a7da7de50b6aa5
Marielaj9 <marielajara567@gmail.com>
2023-03-17 23:39:48 -0300

    se agrega login y register

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, {  useState } from 'react'
+
+import { Link, useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  
+  const navigate= useNavigate();
+  const [formData, setFormData] = useState({
+      usuario: '',
+      password: '',
+  });
+
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+          
+          
+          navigate("/")
+          
+          
+      } catch (error) {
+          console.error(error);
+          setErrorMessage('Credenciales incorrectas');
+          
+      }
+
+  };
   return (
-    <div>LoginPage</div>
+    <div>
+
+      <form onSubmit={handleSubmit} className="login">
+        <h2 className='h2'>Iniciar sesión</h2>
+        <label className='label'>
+          Usuario:
+          <input type="text" name="usuario" className='input' value={formData.usuario} onChange={handleChange} />
+        </label>
+        <br />
+        <label className='label'>
+          Contraseña:
+          <input type="password" name="password" className='input' value={formData.password} onChange={handleChange} />
+        </label>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        <br />
+        <button type="submit" className='button'>Iniciar sesión</button>
+        <Link to='/registro' className='button'>Registrarse</Link>
+      </form>
+
+    </div>
   )
 }
 
