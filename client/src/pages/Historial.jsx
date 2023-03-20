920673e0f8790f535e9c7e25bd9ad40090d57ac5
Ruben Amarilla <daniel.amarilla.rda@gmail.com>
2023-03-20 13:34:39 -0300

    interfaz historial hecho

--- client/src/pages/Historial.jsx
+++ client/src/pages/Historial.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from "react";
+import { Container, CssBaseline, Grid, Typography } from "@mui/material";
 
 const Historial = () => {
+  const historial = [
+    {
+      pasos: 5202,
+      calorías: 231,
+      fecha: "12/01/23",
+      hora: "12:36",
+    },
+    {
+      pasos: 5202,
+      calorías: 231,
+      fecha: "12/01/23",
+      hora: "12:36",
+    },
+    {
+      pasos: 5202,
+      calorías: 231,
+      fecha: "12/01/23",
+      hora: "12:36",
+    },
+    {
+      pasos: 5202,
+      calorías: 231,
+      fecha: "12/01/23",
+      hora: "12:36",
+    },
+    {
+      pasos: 5202,
+      calorías: 231,
+      fecha: "12/01/23",
+      hora: "12:36",
+    },
+    {
+      pasos: 5202,
+      calorías: 231,
+      fecha: "12/01/23",
+      hora: "12:36",
+    },
+  ];
   return (
-    <div>Historial</div>
-  )
-}
+    <>
+      <CssBaseline />
+      <Grid
+        container
+        minWidth="xl"
+        minHeight="md"
 
-export default Historial
+        direction="column"
+        sx={{ bgcolor: "gray", pt: "1rem", paddingBottom: "1rem", padding: "1rem" }}
+      >
+        {historial.map((usuario, idx) => (
+          <Grid
+            container
+            item
+            alignItems={"center"}
+            xs={12}
+            sx={{ bgcolor: "whitesmoke", marginBottom: "1rem", padding: "0.5rem" }}
+          >
+            <Grid item xs={4}>
+              <Typography component="h5" variant="h5">
+                Pasos
+              </Typography>
+              <Typography component="h3" variant="h3">
+                {usuario.pasos}
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography component="h5" variant="h5">
+                Calorías
+              </Typography>
+              <Typography component="h3" variant="h3">
+                {usuario.calorías}
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography component="h3" variant="h5">
+                {usuario.fecha}
+              </Typography>
+              <Typography component="h3" variant="h5">
+                {usuario.hora}
+              </Typography>
+            </Grid>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
+
+export default Historial;
--- client/src/pages/Inicio.jsx
+++ client/src/pages/Inicio.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import { Grid, Typography } from "@mui/material";
 
 const Inicio = () => {
